8d5089a2278deb73a8a7fbf049147c91d130e9d3
indrithasa <hasa.indrit@gmail.com>
2016-11-22 23:11:05 +0100

    ProgramEmail

---
+++ G1/Ushtrime3/ProgramiDergoEmail/ProgramiEmail.NUnitTest/LexoSkedarNunit.cs
@@ -0,0 +1,37 @@
+﻿
+using NUnit.Framework;
+using ProgramiEmailTest;
+
+namespace ProgramiEmail.NUnitTest
+{
+    [TestFixture]
+
+    class LexoSkedarNunit
+    {
+        [Test]
+        public void DuhetKthejeAdresenNunit()
+        {
+            var rezultati = LexoSkedarinTest.DuhetKthejeAdresen();
+            var vlera = "";
+            Assert.That(rezultati, Is.EqualTo(vlera));
+        }
+        [Test]
+        public void DuhetKthejeSubjektinNunit()
+        {
+            var rezultati = LexoSkedarinTest.DuhetKthejeSubjektin();
+            var vlera = "last";
+            Assert.AreNotSame(vlera, rezultati);
+            
+        }
+        [Test]
+        public void DuhetKthejeTrupiNunit()
+        {
+            var rezultati = LexoSkedarinTest.DuhetKthejeTrupin();
+            var vlera = "dorzimdetyre";
+            Assert.That(rezultati, Is.EqualTo(vlera));
+
+        }
+   
+
+    }
+}

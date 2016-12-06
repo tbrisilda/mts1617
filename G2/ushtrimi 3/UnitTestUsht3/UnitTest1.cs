46cb32688a837db989d98d3fe88249b0012c3317
MarenaKuteli <marena.kuteli2@fshnstudent.info>
2016-12-06 12:27:55 +0100

    ushtrimi 3

---
+++ G2/ushtrimi 3/UnitTestUsht3/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using usht3;
+namespace UnitTestUsht3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //string a = "A";
+            //string b = "B";
+            var a = new List<string>() { "A", "B" };
+            var b = new List<string>() { "M", "N", "O", "P", "Q" };
+
+            Random rand = new Random();
+
+            int indexa = rand.Next(a.Count);
+            int indexb = rand.Next(b.Count);
+
+            Class1 test = new Class1();
+            test.usht(a[indexa], b[indexb]);
+
+            //["A","B"],{"M","N","O","P","Q"}
+        }
+    }
+}
---
+++ G2/ushtrimi 3/usht3/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace usht3
+{
+    public class Class1
+    {
+
+        public string usht(string x, string y)
+        {
+            string lista = x + " " + y;
+            return lista;
+        }
+    }
+}
